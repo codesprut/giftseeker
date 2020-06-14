b10cd34931bbc6a65d99ece07ca5f93790b3596a
CodeSprut <Alex-p.s@yandex.ru>
2020-06-15 03:42:02 +0500

    time module refactoring

--- src/electron/web/js/utlis/time.js
+++ src/electron/web/js/utlis/time.js
@@ -9,17 +9,14 @@ class Time {
     const minutes = date.getMinutes();
     const seconds = date.getSeconds();
 
-    format = format.toLowerCase();
-
-    format = format.replace("hh", this.leadZero(hours));
-    format = format.replace("mm", this.leadZero(minutes));
-    format = format.replace("ss", this.leadZero(seconds));
-
-    format = format.replace("h", hours);
-    format = format.replace("m", minutes);
-    format = format.replace("s", seconds);
-
-    return format;
+    return format
+      .toLowerCase()
+      .replace("hh", this.leadZero(hours))
+      .replace("mm", this.leadZero(minutes))
+      .replace("ss", this.leadZero(seconds))
+      .replace("h", hours)
+      .replace("m", minutes)
+      .replace("s", seconds);
   }
 
   leadZero(number) {
