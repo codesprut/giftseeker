982209569e2d4ce6919b33b6d1361d0bc0adae3f
CodeSprut <Alex-p.s@yandex.ru>
2020-06-21 15:31:55 +0500

    ui browser features

---
+++ src/electron/web/js/browser.js
@@ -0,0 +1,46 @@
+const { remote, shell } = require("electron");
+const { Browser, settings, language } = remote.getGlobal("sharedData");
+
+const openUrl = (url, useProgramBrowser) => {
+  if (settings.get("use_system_browser") && !useProgramBrowser)
+    return shell.openExternal(url);
+
+  Browser.loadURL(url);
+  Browser.setTitle("GS Browser - " + language.get("auth.browser_loading"));
+
+  Browser.show();
+};
+
+const runForAuth = async (websiteUrl, authPageUrl, authContent) => {
+  Browser.webContents.on("did-finish-load", () => {
+    if (Browser.getURL().indexOf(websiteUrl) >= 0) {
+      Browser.webContents
+        .executeJavaScript('document.querySelector("body").innerHTML')
+        .then(body => {
+          if (body.indexOf(authContent) >= 0) Browser.close();
+        });
+    }
+  });
+
+  openUrl(authPageUrl, true);
+
+  return new Promise(resolve => {
+    Browser.once("close", () => {
+      Browser.webContents.removeAllListeners("did-finish-load");
+
+      resolve(
+        Browser.webContents.session.cookies
+          .get({ domain: websiteUrl.split("/")[2] })
+          .then(cookies =>
+            cookies.map(cookie => cookie.name + "=" + cookie.value).join("; ")
+          )
+          .catch(() => "")
+      );
+    });
+  });
+};
+
+export default {
+  openUrl,
+  runForAuth
+};
