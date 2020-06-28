3469880cdf038d30d7ac2ce1a8711290c5692608
CodeSprut <Alex-p.s@yandex.ru>
2020-06-28 23:41:01 +0500

    clear-html-tags utility added

---
+++ src/app/utils/clear-html-tags.js
@@ -0,0 +1,17 @@
+/**
+ * Remove tags from html code
+ * @param html
+ * @param tags to be removed
+ * @returns {string}
+ */
+module.exports = (html, tags = []) => {
+  let result = html;
+
+  for (const tag of tags) {
+    const regExp = new RegExp(`<${tag}(\/|\\s.*?)?\>(.*?<\/${tag}>)?`, "g");
+    result = result.replace(regExp, "");
+  }
+
+  return result;
+};
+1;
---
+++ tests/app/utils/clear-html-tags.test.js
@@ -0,0 +1,55 @@
+const clearHtmlTags = require("../../../src/app/utils/clear-html-tags");
+
+const commonHtmlCode =
+  "<html><script></script><hr><input/><span></span><s></s><span></span></html>";
+
+const cases = [
+  {
+    toString: () => "Root node removed",
+    clearTags: ["html"],
+    expected: ""
+  },
+  {
+    toString: () => "Nothing removed if no one tag found",
+    clearTags: ["br", "p", "i"],
+    expected: commonHtmlCode
+  },
+  {
+    toString: () => "Nothing removed if no one tag provided",
+    clearTags: [],
+    expected: commonHtmlCode
+  },
+  {
+    toString: () => "Remove once found tag",
+    clearTags: ["script"],
+    expected: "<html><hr><input/><span></span><s></s><span></span></html>"
+  },
+  {
+    toString: () => "Multiple found tags removed",
+    clearTags: ["span"],
+    expected: "<html><script></script><hr><input/><s></s></html>"
+  },
+  {
+    toString: () => "Multiple found different tags removed",
+    clearTags: ["span", "script"],
+    expected: "<html><hr><input/><s></s></html>"
+  },
+  {
+    toString: () => "Single tag node removed",
+    html: "<html><hr></html>",
+    clearTags: ["hr"],
+    expected: "<html></html>"
+  },
+  {
+    toString: () => "Tag with attributes removed",
+    html:
+      "<html><div></div><span id='to-be-removed' data-remove='true'></span></html>",
+    clearTags: ["span"],
+    expected: "<html><div></div></html>"
+  }
+];
+test.each(cases)("%s", async caseData => {
+  expect(
+    clearHtmlTags(caseData.html || commonHtmlCode, caseData.clearTags)
+  ).toEqual(caseData.expected);
+});
