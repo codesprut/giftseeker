ad5009ddfef537ced29b43e709e42b721af001c5
CodeSprut <Alex-p.s@yandex.ru>
2020-07-03 18:21:22 +0500

    electron browser session code actualize

--- src/app/session.js
+++ src/app/session.js
@@ -1,13 +1,12 @@
 const { session } = require("electron");
 
-const { appName, defaultUseragent } = require("../electron/config");
+const { appName } = require("../electron/config");
 const settings = require("./settings");
 
 const _session = session.fromPartition(`persist:${appName}`);
 _session.setUserAgent(settings.get("user_agent"));
 
 settings.on("change", "user_agent", newUserAgent => {
-  newUserAgent = newUserAgent || defaultUseragent;
   _session.setUserAgent(newUserAgent);
 });
 
