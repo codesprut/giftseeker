5a959306ea7669d2a9b29f0490042a69e738dd3f
CodeSprut <Alex-p.s@yandex.ru>
2020-07-08 21:38:21 +0500

    settings mock get method fixed

--- tests/app/seeker/__mocks/settingsMock.js
+++ tests/app/seeker/__mocks/settingsMock.js
@@ -11,8 +11,10 @@ let currentSettings = {};
 
 settings.set.mockImplementation((key, val) => (currentSettings[key] = val));
 
-settings.get.mockImplementation((key, val) => {
-  if (currentSettings[key] !== undefined) return val;
+settings.get.mockImplementation((key, defaultValue) => {
+  if (currentSettings[key] !== undefined) return currentSettings[key];
+  if (defaultValue !== undefined) return defaultValue;
+
   return false;
 });
 
