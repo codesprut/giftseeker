af41a09db83f77bb4b11f92bed227a545e09f81f
CodeSprut <Alex-p.s@yandex.ru>
2020-08-15 03:01:19 +0500

    steamgifts level tests

--- tests/app/seeker/services/steamgifts.test.js
+++ tests/app/seeker/services/steamgifts.test.js
@@ -1,49 +1,68 @@
-const settingsMock = require("../__mocks/settingsMock");
-const steamGifts = require("../../../../src/app/seeker/services/steamgifts");
+const settingsMock = require('../__mocks/settingsMock')
+const steamGifts = require('../../../../src/app/seeker/services/steamgifts')
 
 const createGiveaway = (number, fields = {}) => {
   return Object.assign(
     {
-      url: "http://sg.com",
+      url: 'http://sg.com',
       cost: 0,
       copies: 1,
       entries: 100,
       timeLeft: 300,
       levelRequired: 0,
       levelPass: true,
-      name: "test " + number,
-      code: "test" + number,
+      name: 'giveaway ' + number,
+      code: 'giveaway' + number,
       entered: false,
-      winChance: 100,
+      winChance: 100
     },
-    fields,
-  );
-};
+    fields
+  )
+}
 
-const defaultValue = 300;
+const defaultValue = 300
 
 const commonGiveaways = [
   createGiveaway(1),
   createGiveaway(2, { entered: true }),
-  createGiveaway(3, { levelPass: false }),
-];
+  createGiveaway(3, { levelPass: false })
+]
 
-describe("Entry logic", () => {
+describe('Entry logic', () => {
   const cases = [
     {
-      toString: () => "Default settings",
-      expectTruthy: ["test1"],
+      toString: () => 'Default settings',
+      expectTruthy: ['giveaway1']
     },
-  ];
-  test.each(cases)("%s", async caseData => {
-    settingsMock.setup(caseData.settings || {});
+    {
+      toString: () => 'Check required level',
+      giveaways: [
+        createGiveaway(1, {
+          levelRequired: 3
+        }),
+        createGiveaway(2, {
+          levelRequired: 4
+        }),
+        createGiveaway(3, {
+          levelRequired: 5
+        })
+      ],
+      settings: {
+        steamgifts_min_level: 4
+      },
+      expectTruthy: ['giveaway2', 'giveaway3']
+    }
+  ]
+  test.each(cases)('%s', async (caseData) => {
+    settingsMock.setup(caseData.settings || {})
     for (const giveaway of caseData.giveaways || commonGiveaways) {
-      steamGifts.setValue(caseData.currentValue || defaultValue);
+      const truthyCodes = caseData.expectTruthy || []
+      steamGifts.setValue(caseData.currentValue || defaultValue)
       const canEnter = steamGifts.canEnterGiveaway(
         giveaway,
-        caseData.wishlistPage || false,
-      );
-      expect(canEnter).toEqual(caseData.expectTruthy.includes(giveaway.code));
+        caseData.wishlistPage || false
+      )
+      expect(canEnter).toEqual(truthyCodes.includes(giveaway.code))
     }
-  });
-});
+  })
+})
