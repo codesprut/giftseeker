6967c5ba53c9b051cc7d06300b98120b276d801c
CodeSprut <Alex-p.s@yandex.ru>
2020-08-16 03:12:23 +0500

    astats getUserInfo implemented

--- src/app/seeker/services/astats.js
+++ src/app/seeker/services/astats.js
@@ -1,4 +1,5 @@
 const Seeker = require("../core");
+const { parse: parseHtml } = require("node-html-parser");
 
 class Astats extends Seeker {
   constructor() {
@@ -6,7 +7,15 @@ class Astats extends Seeker {
       websiteUrl: "https://astats.astats.nl/astats/",
       authPageUrl: "https://astats.astats.nl/astats/profile/Login.php",
       authContent: "Log out",
+      withValue: false,
     });
+  }
+
+  async getUserInfo() {
+    return this.http.get(this.websiteUrl + "User_Info.php").then(response => ({
+      avatar: response.data.match(/"src="(.*?)"/)[1],
+      username: parseHtml(response.data).querySelector("h2").structuredText,
+    }));
   }
 
   async seekService() {}
