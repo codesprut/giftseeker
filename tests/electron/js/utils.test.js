7519d809928cd98936da7a6447825c8f0d33ce98
CodeSprut <Alex-p.s@yandex.ru>
2020-09-26 02:57:49 +0500

    elapsed time method testcases

---
+++ tests/electron/js/utils.test.js
@@ -0,0 +1,32 @@
+import time from "../../../src/electron/web/js/utlis/time";
+
+describe("Elapsed method", () => {
+  const cases = [
+    {
+      toString: () => "Works correctly with time less an minute",
+      timestamp: 59,
+      expected: "59",
+    },
+    {
+      toString: () => "Works correctly with time less an hour",
+      timestamp: 125,
+      expected: "02:05",
+    },
+    {
+      toString: () => "Works correctly with time over an hour",
+      timestamp: 3669,
+      expected: "01:01:09",
+    },
+    {
+      toString: () => "Works correctly with time over an hour:case 2",
+      timestamp: 3609,
+      expected: "01:00:09",
+    },
+  ];
+
+  test.each(cases)("%s", async caseData => {
+    const elapsed = time.elapsed(caseData.timestamp);
+
+    expect(elapsed).toBe(caseData.expected);
+  });
+});
