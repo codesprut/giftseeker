dc7829a64de46016817ad76a859643a69fd30d5c
codesprut <alex-p.s@yandex.ru>
2021-11-23 22:24:22 +0500

    fix: nix os exec path detections; solves #17

--- src/environment.js
+++ src/environment.js
@@ -3,7 +3,7 @@ const path = require("path");
 const isPortable = process.env.PORTABLE_EXECUTABLE_DIR !== undefined;
 const execPath = isPortable
   ? `${process.env.PORTABLE_EXECUTABLE_DIR}\\`
-  : process.execPath.match(/.*\\/i)[0];
+  : process.cwd();
 
 module.exports = {
   isPortable,
