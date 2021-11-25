1f9f61130112d1dd9daeb6d1bb63ce67fe512f39
codesprut <alex-p.s@yandex.ru>
2021-11-26 01:15:36 +0500

    feat: simple cli - wip

--- src/console/main.js
+++ src/console/main.js
@@ -1,1 +1,18 @@
-process.exit(0);
+const { createInterface } = require("readline");
+
+const cli = createInterface(process.stdin, process.stdout);
+
+cli.setPrompt("");
+cli.prompt();
+
+cli
+  .on("line", input => {
+    if (input === "exit") {
+      cli.close();
+    }
+
+    console.error(`No '${input}' command found;`);
+
+    cli.prompt();
+  })
+  .on("close", () => process.exit(0));
