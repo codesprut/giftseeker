80bcf81300dd4abb904371744c7e696ca91a2685
codesprut <alex-p.s@yandex.ru>
2021-11-26 11:47:28 +0500

    fix: default translation

--- src/config.js
+++ src/config.js
@@ -13,7 +13,7 @@ module.exports = {
   websiteUrl: "https://giftseeker.ru/",
   storageDataPath,
   defaultSettings: {
-    language: "en_US",
+    translation: "en_US",
     user_agent:
       "Mozilla/5.0 (iPhone; CPU iPhone OS 13_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/83.0.4103.88 Mobile/15E148 Safari/604.1",
   },
