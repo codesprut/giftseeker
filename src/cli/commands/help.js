294a64df4309dbd771d7b70a8cbaac60a2b9bc16
codesprut <alex-p.s@yandex.ru>
2021-11-28 01:43:54 +0500

    feat: help command now accept filter argument

--- src/cli/commands/help.js
+++ src/cli/commands/help.js
@@ -2,14 +2,20 @@ const { Table } = require("console-table-printer");
 const baseCommand = require("./base-command");
 
 module.exports = commands =>
-  baseCommand("help|list", "Displays all available commands", () => {
-    const table = new Table({
-      columns: [{ name: "signature", alignment: "left", color: "green" }],
-    });
+  baseCommand(
+    "help|list",
+    "Displays all available commands. Provide argument for filter e.g., help session",
+    args => {
+      const table = new Table({
+        columns: [{ name: "signature", alignment: "left", color: "green" }],
+      });
 
-    commands.forEach(({ signature, description }) => {
-      table.addRow({ signature, description });
-    });
+      commands
+        .filter(command => (args[0] ? command.signature.match(args[0]) : true))
+        .forEach(({ signature, description }) => {
+          table.addRow({ signature, description });
+        });
 
-    table.printTable();
-  });
+      table.printTable();
+    },
+  );
