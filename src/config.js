325b4020d5dbd6a86c1cb9d6231391d4f36db306
codesprut <alex-p.s@yandex.ru>
2025-02-16 03:37:48 +0500

    feat: useragent updated

--- src/config.js
+++ src/config.js
@@ -15,6 +15,6 @@ module.exports = {
   defaultSettings: {
     translation: "en_US",
     user_agent:
-      "Mozilla/5.0 (iPhone; CPU iPhone OS 16_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/106.0.5249.92 Mobile/15E148 Safari/604.1",
+      "Mozilla/5.0 (iPhone; CPU iPhone OS 17_7 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/133.0.6943.84 Mobile/15E148 Safari/604.1",
   },
 };
